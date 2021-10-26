ced977c62b0f4caeb221bf714489ec9c77f49eab
OksanaBakl <oksanamolesha@ukr.net>
2021-10-26 16:21:32 +0300

    Updates

---
+++ static/js/not-found.66999312.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[6],{100:function(t,e,i){"use strict";i.r(e);var s=i(1);e.default=function(){return Object(s.jsx)("h1",{children:"404 Page is not found"})}}}]);
+//# sourceMappingURL=not-found.66999312.chunk.js.map
